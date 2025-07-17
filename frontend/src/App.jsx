bd2fe62d07619078c8772e7b763f160915b34983
Hanuna23 <hananelguechatiu@gmail.com>
2025-07-17 19:00:55 +0200

    Eliminar submódulo frontend/aynix forzadamente

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 to-pink-500 text-white text-3xl font-bold">
+      ¡Hola, Tailwind está funcionando! 🚀
+    </div>
+  )
+}
+
+export default App;
+
